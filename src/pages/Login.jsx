eb44c22ea21883d996e25704cf555fe16c8df0f2
NeamotHossainNadim <nadimmir444@gmail.com>
2025-11-21 05:23:41 +0600

    create All Movies page with grid layout and dynamic movie cards

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,7 +15,7 @@ export default function Login() {
 
     try {
       await loginUser(email, password);
-      toast.success("Login successful ✅");
+      toast.success("Login successful");
       navigate("/");
     } catch (error) {
       toast.error(error.message);
@@ -25,16 +25,16 @@ export default function Login() {
 return (
   <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-indigo-100 via-white to-blue-100 px-4">
     <div className="w-full max-w-md bg-white/80 backdrop-blur-lg p-8 rounded-3xl shadow-xl border border-white/30">
-      {/* Header */}
+   
       <h2 className="text-3xl font-extrabold text-center text-gray-800">
-        Welcome Back 👋
+        Welcome Back 
       </h2>
       <p className="text-center text-sm text-gray-500 mt-1 mb-6">
         Login to continue to MovieMaster
       </p>
 
       <form onSubmit={handleLogin} className="space-y-4">
-        {/* Email */}
+      
         <div>
           <label className="block text-sm font-medium text-gray-600 mb-1">
             Email
@@ -48,7 +48,6 @@ return (
           />
         </div>
 
-        {/* Password */}
         <div>
           <label className="block text-sm font-medium text-gray-600 mb-1">
             Password
@@ -62,12 +61,10 @@ return (
           />
         </div>
 
-        {/* Button */}
         <button className="w-full bg-gradient-to-r from-indigo-600 to-blue-600 hover:from-indigo-700 hover:to-blue-700 text-white py-3 rounded-xl font-semibold tracking-wide transition transform hover:-translate-y-0.5 active:translate-y-0 shadow-lg">
           Login
         </button>
 
-        {/* Footer */}
         <p className="mt-4 text-center text-sm text-gray-600">
           Don&apos;t have an account?{" "}
           <Link
