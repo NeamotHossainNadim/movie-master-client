26b9313b057a1c15b126e0ba1ba4c9936f9873d8
NeamotHossainNadim <nadimmir444@gmail.com>
2025-11-21 05:06:42 +0600

    implement My Collection page showing movies added by logged-in user

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -17,27 +17,22 @@ export default function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  // ✅ Register
   const createUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
-  // ✅ Login
   const loginUser = (email, password) => {
     return signInWithEmailAndPassword(auth, email, password);
   };
 
-  // ✅ Google Sign In
   const googleLogin = () => {
     return signInWithPopup(auth, googleProvider);
   };
 
-  // ✅ Logout
   const logOut = () => {
     return signOut(auth);
   };
 
-  // ✅ Track User
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
